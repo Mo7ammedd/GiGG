628d806d54357e70449dbdb4efa19d5f551a78b4
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-08-10 23:32:48 +0300

    Init

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const lyricsRoutes = require('./routes/lyricsRoutes'); 
-const { errorHandler } = require('./utils/errorHandler');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const lyricsRoutes = require("./routes/lyricsRoutes");
+const { errorHandler } = require("./utils/errorHandler");
 
 dotenv.config();
 
@@ -14,12 +14,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api', lyricsRoutes); 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api", lyricsRoutes);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+console.log("Hello");
