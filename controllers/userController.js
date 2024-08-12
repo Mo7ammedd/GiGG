6d166577c09efb647c4946ab6a9af99a498d02fe
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-08-12 16:51:39 +0300

    add me

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,3 +59,18 @@ exports.updateUserPhone = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+//getUserProfile
+exports.getUserProfile = async (req, res) => {
+  try {
+      const user = await User.findById(req.user._id).select('-password'); // Exclude the password field
+
+      if (user) {
+          res.json(user);
+      } else {
+          res.status(404).json({ message: 'User not found' });
+      }
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { changePassword, updateUserEmail, updateUserPhone } = require('../controllers/userController');
+const { changePassword, updateUserEmail, updateUserPhone, getUserProfile} = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.put('/change-password', protect, changePassword);
 router.put('/change-email', protect, updateUserEmail);
 router.put('/change-phone', protect, updateUserPhone);
+router.get('/me', protect, getUserProfile);
+
 
 module.exports = router;
