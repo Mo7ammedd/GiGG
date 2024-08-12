25f63461834d01eaeeff5bab75a3635e2140f634
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-08-12 17:11:13 +0300

    add me

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,3 +74,26 @@ exports.getUserProfile = async (req, res) => {
       res.status(500).json({ message: error.message });
   }
 };
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a specific user
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
