4b2606c16652ea8539defeefc58e0894e383a813
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-08-12 03:18:01 +0300

    upload changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const { changePassword, updateUserEmail, updateUserPhone, uploadImage, getAllUsers, getUserById } = require('../controllers/userController');
-const { protect } = require('../middlewares/authMiddleware');
-const upload = require('../middlewares/multerMiddleware'); 
+const express = require("express");
+const {
+  changePassword,
+  updateUserEmail,
+  updateUserPhone,
+  uploadImage,
+  getAllUsers,
+  getUserById,
+} = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/multerMiddleware");
 const router = express.Router();
 
 // Protected routes
-router.put('/change-password', protect, changePassword);
-router.put('/change-email', protect, updateUserEmail);
-router.put('/change-phone', protect, updateUserPhone);
-router.post('/upload-img', protect, upload.single('image'), uploadImage);
-router.get('/all', protect, getAllUsers); 
-router.get('/:id', protect, getUserById); 
-
-
+router.put("/change-password", protect, changePassword);
+router.put("/change-email", protect, updateUserEmail);
+router.put("/change-phone", protect, updateUserPhone);
+router.post("/upload-img", protect, upload.single("image"), uploadImage);
+router.get("/all", protect, getAllUsers);
+router.get("/:id", protect, getUserById);
 
 module.exports = router;
