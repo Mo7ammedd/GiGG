dccdb88e87b19d5f5d957dccae70e6602338da08
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-08-24 16:13:28 +0300

    OTP VERIFICATION

--- controllers/authController.js
+++ controllers/authController.js
@@ -152,7 +152,6 @@ exports.resendOTP = async (req, res) => {
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await User.findOne({ email });
 
