0c56144875b493f17961ec4cfc86dc59fec58735
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-08-25 03:23:27 +0300

    yarab tany tany

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("../models/User");
+const { User } = require("../models/User");
 
 module.exports = function () {
   passport.use(
